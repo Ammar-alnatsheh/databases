884d2498f23c481b124bfad5b8759c17eb9ffcca
Ammar-alnatsheh <ammar.alnatsheh@gmail.com>
2018-12-05 21:04:54 -0800

    working on post and get functions

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,21 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (callback) {
+      db.serialize(function() {
+        db.all("SELECT * FROM messages", function(err, allRows) {
+          
+          if (err != null) {
+            console.log(err);
+            callback(err);
+          }
+          console.log(util.inspect(allRows));
+          callback(allRows);
+          db.close();
+        });
+      });
+
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
@@ -13,3 +27,26 @@ module.exports = {
   }
 };
 
+
+
+// var readRecordsFromMediaTable = function(){
+//   return new Promise(function (resolve, reject) {
+//     var responseObj;
+//     db.all("SELECT * FROM MediaTable", null, function cb(err, rows) {
+//       if (err) {
+//         responseObj = {
+//           'error': err
+//         };
+//         reject(responseObj);
+//       } else {
+//         responseObj = {
+//           statement: this,
+//           rows: rows
+//         };
+//         resolve(responseObj);
+//       }
+//       db.close();
+//     });
+//   });
+// }
+//https://stackoverflow.com/questions/39106668/node-js-sqlite3-read-all-records-in-table-and-return
