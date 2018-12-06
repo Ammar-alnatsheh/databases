958b31ca5a2f58a6903308721237aef503a12dcc
Ammar-alnatsheh <ammar.alnatsheh@gmail.com>
2018-12-05 17:27:47 -0800

    created database n tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id integer PRIMARY KEY,
+  name char
 );
+
+CREATE TABLE rooms (
+  id integer PRIMARY KEY,
+  name char
+);
+
+CREATE TABLE messages (
+  id integer PRIMARY KEY,
+  user_id integer,
+  room_id integer,
+  FOREIGN KEY(user_id) REFERENCES users (id),
+  FOREIGN KEY(room_id) REFERENCES rooms (id),
+  message text
+);
+
 
 /* Create other tables and define schemas for them here! */
 
@@ -12,6 +28,6 @@ CREATE TABLE messages (
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
