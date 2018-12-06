ca2b43da0bc3404dbb4d8ae63ab79a15c2c9a962
Ammar-alnatsheh <ammar.alnatsheh@gmail.com>
2018-12-06 12:38:48 -0800

    structure the controller data flow

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,30 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      //send the request to db models with get action to messages table
+      models.messages.get(res);
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      //send the request to db models with post action to messages table
+      models.messages.post(res);
+      
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      //send the request to db models with get action to user table
+      models.users.get(res);
+
+    },
+    post: function (req, res) {
+      //send the request to db models with post action to user table
+      models.users.post(res);
+
+    }
   }
 };
 
